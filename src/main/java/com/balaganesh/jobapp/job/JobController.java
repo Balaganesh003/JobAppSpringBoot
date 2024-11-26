336c087119a2d671806e0b2f73ccf27835aa73de
Balaganesh K <k.balaganesh26@gmail.com>
2024-11-26 21:25:11 +0530

    Initial Project

--- src/main/java/com/balaganesh/jobapp/job/JobController.java
+++ src/main/java/com/balaganesh/jobapp/job/JobController.java
@@ -1,10 +1,10 @@
-package com.balaganesh.jobapp;
+package com.balaganesh.jobapp.job;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class JobAppController {
+public class JobController {
     @GetMapping("/")
     public String index() {
         return "index";
